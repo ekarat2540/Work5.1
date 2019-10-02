556cbaccbca7f2da1c33d63d03215bb281c29ec2
Ekarat Sukprasert <59160993@go.buu.ac.th>
2019-10-02 23:12:29 +0700

    add view model data binding

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -56,9 +56,7 @@ class GameFragment : Fragment() {
         viewModel.resetList()
         viewModel.nextWord()
 
-        binding.correctButton.setOnClickListener { onCorrect() }
-        binding.skipButton.setOnClickListener { onSkip() }
-        binding.endGameButton.setOnClickListener{ onEndGame()}
+        binding.gameViewModel = viewModel
 
         viewModel.score.observe(this, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
@@ -75,9 +73,6 @@ class GameFragment : Fragment() {
 
     }
 
-    private fun onEndGame() {
-        gameFinished()
-    }
 
     private fun gameFinished(){
         Toast.makeText(activity,"Game has just finished", Toast.LENGTH_SHORT).show()
@@ -87,18 +82,7 @@ class GameFragment : Fragment() {
         viewModel.onGameFinishComplete()
     }
 
-    private fun onSkip() {
-        viewModel.onSkip()
 
-
-    }
-
-    private fun onCorrect() {
-        viewModel.onCorrect()
-
-
-
-    }
 
     /**
      * Resets the list of words and randomizes the order
